6a2bc2fc7b0b4ef6e5b8806e6ededda6ec103cec
VicGanoh <kofiganoh@gmail.com>
2024-11-28 06:58:41 -0800

    consume message from queues

--- amqp_client/rabbitmq/receive.py
+++ amqp_client/rabbitmq/receive.py
@@ -5,3 +5,18 @@ connection = pika.BlockingConnection(connection_param)
 channel = connection.channel()
 
 channel.queue_declare(queue="hello")
+
+# callback function to handle incoming messages
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+# start consuming messages
+channel.basic_consume(
+    queue="hello",
+    auto_ack=True,
+    on_message_callback=callback,
+)
+
+print(" [*] Waiting for messages. To exit press CTRL+C")
+channel.start_consuming()
+
