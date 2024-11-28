193e710e427c101b912e315332bec1c5b1797754
VicGanoh <kofiganoh@gmail.com>
2024-11-28 06:23:30 -0800

    add connection and queue

--- amqp_client/rabbitmq/receive.py
+++ amqp_client/rabbitmq/receive.py
@@ -1,2 +1,7 @@
 import pika
 
+connection_param = pika.ConnectionParameters("localhost")
+connection = pika.BlockingConnection(connection_param)
+channel = connection.channel()
+
+channel.queue_declare(queue="hello")
