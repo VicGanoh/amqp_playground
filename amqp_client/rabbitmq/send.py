31e315af294a178d90ca299202e30d964c6f9b20
VicGanoh <kofiganoh@gmail.com>
2024-11-28 05:47:36 -0800

    add queue

--- amqp_client/rabbitmq/send.py
+++ amqp_client/rabbitmq/send.py
@@ -3,3 +3,5 @@ import pika
 # establish connection to the RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
+
+channel.queue_declare(queue="hello")
