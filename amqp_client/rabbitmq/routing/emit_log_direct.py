d51c203664f546c87d1679cdf811599b68b31884
VicGanoh <kofiganoh@gmail.com>
2024-11-28 11:40:59 -0800

    add logs

--- amqp_client/rabbitmq/routing/emit_log_direct.py
+++ amqp_client/rabbitmq/routing/emit_log_direct.py
@@ -6,10 +6,15 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
 
-message = " ".join(sys.argv[1:]) or "info: Hello, World!"
+severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
 
 channel.basic_publish(
     exchange="direct_logs",
-    routing_key="severity",
+    routing_key=severity,
     body=message,
 )
+
+print(f" [x] Sent {severity}:{message}")
+
+connection.close()
--- amqp_client/rabbitmq/routing/receive_logs_direct.py
+++ amqp_client/rabbitmq/routing/receive_logs_direct.py
@@ -4,6 +4,8 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
 
+channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
+
 result =channel.queue_declare(queue="", exclusive=True)
 queue_name = result.method.queue
 
@@ -26,8 +28,8 @@ print(' [*] Waiting for logs. To exit press CTRL+C')
 def callback(ch, method, properties, body):
     print(f" [x] {method.routing_key}:{body}")
 
-channel.basic_consume(
-    queue=queue_name,
-    on_message_callback=callback,
-)
 
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
