81ce5a8bb038e27c407a8e334d30a576887a2af3
VicGanoh <kofiganoh@gmail.com>
2025-02-09 19:29:45 +0000

    feat: add order event publisher

--- amqp_client/rabbitmq/topics/publisher.py
+++ amqp_client/rabbitmq/topics/publisher.py
@@ -1,6 +1,7 @@
+from typing import Any
 import pika
 
-class Publisher:
+class OrderPublisher:
     def __init__(self):
         # Step 1: Initialize connection parameters
         self.connection = pika.BlockingConnection(
@@ -15,4 +16,31 @@ class Publisher:
             exchange_type="topic",
             durable=True
         )
+
+    def publish_order_event(self, order_data: dict[str, Any], event_type: str):
+        """
+        Publish order events with different routing keys based on event type (e.g. "order.created", "order.updated", "order.cancelled")
+        """
+        try:
+            # Create routing key based on event type category
+            routing_key = f"order.{event_type}.{orde_data['category']}"
+
+            # Convert order data to JSON
+            message = json.dumps(order_data)
+
+            # Publish message with persistance
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key=routing_key,
+                body=message,
+                properties=pika.BasicProperties(
+                    delivery_mode=2, # make delivery persistent
+                    content_type="application/json",
+                    content_encoding="utf-8"
+                )
+            )
+        except Exception as e:
+            print(f"Error publishing order event: {e}")
     
+    def close(self):
+        self.connection.close()
